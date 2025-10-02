fa77df895f5e8892041f08024375b2b41fa50929
lucio1962 <lucianosmriglio@gmail.com>
2025-10-02 08:45:53 +0200

    Create menu-loader.js

---
+++ menu-loader.js
@@ -0,0 +1,20 @@
+// Carica menu.json e genera HTML
+async function loadMenu(limitFree=false){
+  try {
+    const res = await fetch("menu.json");
+    const data = await res.json();
+    let html="";
+    data.forEach(cat=>{
+      html+=`<h4>${cat.cat}</h4><ul>`;
+      let items=cat.items;
+      if(limitFree) items=items.slice(0,3);
+      items.forEach(i=>{
+        html+=`<li>${i.name} – €${i.price}</li>`;
+      });
+      html+="</ul>";
+    });
+    return html;
+  } catch(e){
+    return "<p>Errore caricamento menu</p>";
+  }
+}
